1a6685346a5b382a7422de0a859b244158ac92c1
Jcmendes7 <josecarlosms7f@gmail.com>
2026-04-08 09:43:07 -0300

    Removendo .env do repositório

--- src/config/database.js
+++ src/config/database.js
@@ -1,18 +1,12 @@
 const mysql = require("mysql2");
+require("dotenv").config();
 
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "1602",
-  database: "iot_residuos",
+const database = mysql.createConnection({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
 });
 
-db.connect((err) => {
-  if (err) {
-    console.error("Erro ao conectar:", err);
-  } else {
-    console.log("MySQL conectado");
-  }
-});
-
-module.exports = db;
+module.exports = database;
