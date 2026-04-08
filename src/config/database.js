a9e9dec5380ec8bf9765a6817adee06683aef9b4
Jcmendes7 <josecarlosms7f@gmail.com>
2026-04-08 10:29:33 -0300

    config banco local e remoto

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,38 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-const database = mysql.createConnection({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT,
+let db;
+
+if (process.env.MYSQL_URL) {
+  console.log("Usando banco remoto (Railway)");
+
+  const url = new URL(process.env.MYSQL_URL);
+
+  db = mysql.createConnection({
+    host: url.hostname,
+    user: url.username,
+    password: url.password,
+    database: url.pathname.replace("/", ""),
+    port: url.port,
+  });
+} else {
+  console.log("Usando banco local");
+
+  db = mysql.createConnection({
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT,
+  });
+}
+
+db.connect((err) => {
+  if (err) {
+    console.error("Erro ao conectar:", err);
+  } else {
+    console.log("MySQL conectado");
+  }
 });
 
-module.exports = database;
+module.exports = db;
